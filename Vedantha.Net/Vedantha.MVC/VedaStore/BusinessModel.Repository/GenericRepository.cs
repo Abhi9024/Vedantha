c1fee6f4bc7c90dc1bf96e1987cdfb9558445360
Abhi9024 <gr.abishek.gr@gmail.com>
2016-02-24 00:12:50 +0530

    Added Generic Repository

---
+++ Vedantha.Net/Vedantha.MVC/VedaStore/BusinessModel.Repository/GenericRepository.cs
@@ -0,0 +1,56 @@
+﻿using BusinessModel.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data;
+
+namespace BusinessModel.Repository
+{
+    public class GenericRepository<T>:IGenericRepository<T> where T:class 
+    {
+        readonly pubsContext _pubsContext;
+        public GenericRepository(pubsContext pubsContext)
+        {
+            _pubsContext = pubsContext;
+        }
+
+
+        public void Create(T obj)
+        {
+            if (_pubsContext.Entry<T>(obj).State == EntityState.Detached)
+                _pubsContext.Entry<T>(obj).State = EntityState.Added;
+            else
+                _pubsContext.Set<T>().Attach(obj);
+
+            _pubsContext.SaveChanges();
+        }
+
+        public T Read(object id)
+        {
+            var entity = _pubsContext.Set<T>().Find(id);
+            return entity;
+        }
+
+        public IList<T> Find(System.Linq.Expressions.Expression<Func<T, bool>> creteria)
+        {
+            return _pubsContext.Set<T>().Where(creteria).ToList();
+        }
+
+        public void Update(T obj)
+        {
+            _pubsContext.Set<T>().Attach(obj);
+            _pubsContext.Entry<T>(obj).State = EntityState.Modified;
+
+            _pubsContext.SaveChanges();
+        }
+
+        public void Delete(object id)
+        {
+            _pubsContext.Set<T>().Remove(Read(id));
+            _pubsContext.SaveChanges();
+        }
+    }
+}
---
+++ Vedantha.Net/Vedantha.MVC/VedaStore/BusinessModel.Repository/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessModel.Repository
+{
+    interface IGenericRepository<T> where T:class
+    {
+        void Create(T obj);
+        T Read(object id);
+        IList<T> Find(Expression<Func<T,bool>> creteria);
+        void Update(T obj);
+        void Delete(object id);
+    }
+}
